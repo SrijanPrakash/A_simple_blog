5816a1792b34a0ccab9c817e7c3a43890c0fb57b
SrijanPrakash <luckydude632@gmail.com>
2018-01-21 18:02:20 +0530

    A_blog' .

---
+++ mysite/personal/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def index (request):
+	return render(request, 'personal/home.html') 
+	
+	
+def contact (request):
+	return render(request, 'personal/basic.html', {'content' : ['if you like to contact me email me','sijanbibhu@gmail.com']}) 
